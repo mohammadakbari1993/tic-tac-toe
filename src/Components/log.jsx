211609575ec9a8e378d3e7e5715ab3d656b80958
Mohammad Akbari <mohammad.a@nizek.com>
2024-11-05 17:23:10 +0330

    Log is works currectly

--- src/Components/log.jsx
+++ src/Components/log.jsx
@@ -1,11 +1,13 @@
 
 export default function Log({turns}) {
-    return 
-    (<ol id="log">
-             {turns.map((turn) => {
+    
+    return (
+        <ol id="log">
+             {turns.map((turn) => (
                  <li key={`${turn.square.row}${turn.square.col}`}>
                      {turn.player} Selected {turn.square.row}, {turn.square.col}
                 </li>
-            })}
-    </ol>);
+            ))}
+        </ol>
+    );
 }
